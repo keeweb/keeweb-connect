f4cbe20bd421b6848bbcd5b7a88efb2b0079aa12
antelle <dmitryvit@gmail.com>
2021-04-17 17:36:33 +0200

    fixed web connection

--- src/common/features.ts
+++ src/common/features.ts
@@ -1,6 +1,6 @@
 const isSafari = location.origin.includes('safari');
 
 export const supportsUnicodeMenus = !isSafari;
-export const canUseOnlyAppConnection = true;
+export const canUseOnlyAppConnection = isSafari;
 export const canEditShortcuts = !isSafari;
 export const needRequestPermissionsPerSite = isSafari;
